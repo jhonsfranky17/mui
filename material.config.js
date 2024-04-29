90ec0a4c97c21de4f0846f8d9948b13e553960c4
jhonsfranky17 <franklinjettyjohnson6@gmail.com>
2024-04-29 12:39:39 +0530

    Feat: Customize theme

---
+++ material.config.js
@@ -0,0 +1,11 @@
+import { createTheme } from "@mui/material";
+export const theme = createTheme({
+  palette: {
+    primary: {
+      main: "#D81F48",
+    },
+    secondary: {
+      main: "#0C2333",
+    },
+  },
+});
