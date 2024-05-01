34893825cb52bcb79cdad2c459029e6c335395e8
jhonsfranky17 <franklinjettyjohnson6@gmail.com>
2024-05-01 12:01:59 +0530

    Style: Button style define

--- material.config.js
+++ material.config.js
@@ -1,8 +1,18 @@
-import { createTheme } from "@mui/material/styles";
+import { createTheme } from "@mui/material";
 const theme = createTheme({
+  components: {
+    MuiButton: {
+      styleOverrides: {
+        root: {
+          borderRadius: "50px",
+        },
+      },
+    },
+  },
   palette: {
     primary: {
       main: "#D81F48",
+      contrastText: "#ccc",
     },
     secondary: {
       main: "#0C2333",
